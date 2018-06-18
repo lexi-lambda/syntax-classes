2455567384c148ec6f96996b32990e08a6435338
Milo Turner <iitalics@gmail.com>
2018-06-18 17:48:11 -0400

    Add intdef-ctx argument to local-value

--- syntax-classes-lib/syntax/parse/class/local-value.rkt
+++ syntax-classes-lib/syntax/parse/class/local-value.rkt
@@ -10,10 +10,12 @@
     (struct unbound-value ())
     (unbound-value)))
 
-(define-syntax-class (local-value [predicate? (const #t)] #:failure-message [message #f])
+(define-syntax-class (local-value [predicate? (const #t)]
+                                  [intdef-ctx #f]
+                                  #:failure-message [message #f])
   #:description #f
   #:attributes [local-value]
   [pattern id:id
-    #:attr local-value (syntax-local-value #'id (const unbound-value))
+    #:attr local-value (syntax-local-value #'id (const unbound-value) intdef-ctx)
     #:fail-when (eq? (attribute local-value) unbound-value) message
     #:fail-unless (predicate? (attribute local-value)) message])
